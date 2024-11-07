83f9b6b3c6738baf997a368999829c9d1870099e
CrabNo13 <crabeperke2572@gmail.com>
2024-11-07 11:21:26 -0300

    TsParticles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
 import './App.css'
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 import { Tv } from '../elements/Tv'
 import { Phone } from '../elements/Phone'
 import { Navbar } from '../components/navbar'
@@ -11,32 +13,115 @@ import { ProjectItems } from '../elements/ProjectItems'
 import { Skills } from '../elements/Skills'
 import { SkillIcons } from '../elements/SkillIcons'
 import { Experience } from '../elements/Experience'
-
 function App() {
+  const [init, setInit] = useState(false);
 
-  return (
-    <>
-      <HomePage />
-      <div className='aboutSection'>
-        <AboutMe />
-        <Tv />
-      </div>
-      <div className='contactSection'>
-        <PhoneMe />
-        <Phone />
-      </div>
-      <div className='projectsSection'>
-        <Projects />
-        <ProjectItems />
-      </div>
-      <div className='skillsSection'>
-        <Skills />
-        <SkillIcons />
-      </div>
-      <Experience />
-      <Navbar />
-    </>
-  )
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => ({
+      background: {
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: false,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 120,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#be32ff",
+        },
+        links: {
+          enable: false,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "out",
+          },
+          random: false,
+          speed: 3,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 25,
+        },
+        opacity: {
+          value: 0.7,
+        },
+        shape: {
+          type: "triangle",
+        },
+        size: {
+          value: { min: 7, max: 14 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+  if (init) {
+    return (
+      <>
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options}
+        />
+        <HomePage />
+        <div className='aboutSection'>
+          <AboutMe />
+          <Tv />
+        </div>
+        <div className='contactSection'>
+          <PhoneMe />
+          <Phone />
+        </div>
+        <div className='projectsSection'>
+          <Projects />
+          <ProjectItems />
+        </div>
+        <div className='skillsSection'>
+          <Skills />
+          <SkillIcons />
+        </div>
+        <Experience />
+        <Navbar />
+      </>
+    )
+  }
 }
 
 export default App
